21dc9744b9b3352da0293eda8b42ef09922e0f11
Alex <revolalex@gmail.com>
2020-07-08 07:30:56 +0200

    first commit

--- app.js
+++ app.js
@@ -1,25 +1,94 @@
 const http = require('http');
 const { parse } = require('querystring');
-var fs = require("fs");
+const fs = require("fs");
+const ejs = require('ejs');
 
+let nomArray = []
+
+/**
+ * @summary upadate the frond end (GET)
+ * @param {*} res 
+ */
+function updateUI(res) {
+    fs.readFile("home.ejs", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        let htmlContent = fs.readFileSync(__dirname + '/home.ejs', 'utf8');
+
+        // allow me use ejs template code
+        let htmlRenderized = ejs.render(htmlContent, { nomArray: nomArray})
+        res.write(htmlRenderized);
+        return res.end();
+      });
+}
+
+/**
+ * @summary add a student to the list
+ * @param {*} result 
+ * @param {*} res 
+ */
+function addPerson(result, res) {
+    //clear file
+    fs.unlink('data.json', ()=> {})
+
+    var person = {name: result.name, techno: null}
+    nomArray.push(person)
+    // transforme la saisie en json
+    let data = JSON.stringify(nomArray);
+    console.log(data)
+    // ecrire le fichier json en ajoutant
+    fs.appendFileSync('data.json', data);
+
+    updateUI(res);
+}
+
+function addTechno(result, res) {
+    //clear file
+    fs.unlink('data.json', ()=> {})
+    // find the person with no techno
+    let person = nomArray.find( person => person.techno == null)
+    // assign the techno to available person
+    if(person == null) {
+        //TODO: show beautiful modal
+        console.log('Oops,  no students left, come later!');
+    } else {
+        person.techno = result.techno
+        //TODO: change style for person.name
+        console.log(nomArray);
+    
+    // transforme la saisie en json
+    let data = JSON.stringify(nomArray);
+    console.log(data)
+    // ecrire le fichier json en ajoutant
+    fs.appendFileSync('data.json', data);
+    // update 
+    updateUI(res);
+    }
+}
 
 const server = http.createServer((req, res) => {
+    //POST
+    
     if (req.method === 'POST') {
         collectRequestData(req, result => {
-            console.log(result);
-            // transforme la saisie en json
-            let data = JSON.stringify(result)
-            // ecrire le fichier json 
-            fs.writeFileSync('data.json', data);
-            // const newData= fs.readFile(data)
-  
+            if (result.name != null) {
+                addPerson(result, res)
+            } 
+
+            if (result.techno != null) {
+                addTechno(result, res)
+            }
         });
         
-    } 
-    else {
+    } else {
+        //GET, PUT, DELETE (NOT POST!!!)
         fs.readFile("home.ejs", function (err, data) {
             res.writeHead(200, { "Content-Type": "text/html" });
-            res.write(data);
+
+            var htmlContent = fs.readFileSync(__dirname + '/home.ejs', 'utf8');
+
+            var htmlRenderized = ejs.render(htmlContent, {nomArray: nomArray})
+            
+            res.write(htmlRenderized);
             return res.end();
           });
 
@@ -40,8 +109,7 @@ function collectRequestData(request, callback) {
         request.on('end', () => {
             callback(parse(body));
         });
-    }
-    else {
+    } else {
         callback(null);
     }
 }
